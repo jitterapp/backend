5628ed7b8507f8b211ed0e7dee07f97e6d267dad
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-02-16 02:17:34 -0500

    switch to a async await

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const User = require('./user/User');
+const User = require('./User');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.post('/api/1.0/users', (req, res) => {
+router.post('/api/1.0/users', async (req, res) => {
   const saltRounds = 10;
-  bcrypt.hash(req.body.password, saltRounds).then((hash) => {
-    const user = { ...req.body, password: hash };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
+  const hash = await bcrypt.hash(req.body.password, saltRounds);
+  const user = { ...req.body, password: hash };
+  await User.create(user);
+  return res.send({ message: 'User created' });
 });
+
+module.exports = router;
