e7fb50a0d15cc0481bb3f253e0be83fbd1a8b77d
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-02-18 21:29:45 -0500

    remove module

--- __test__/UserRegister.spec.js
+++ __test__/UserRegister.spec.js
@@ -2,7 +2,6 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
-const EmailService = require('../src/email/EmailService');
 const SMTPServer = require('smtp-server').SMTPServer;
 
 let lastMail, server;
