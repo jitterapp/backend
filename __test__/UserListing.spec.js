ec38c4bd27a77728404793f5c3e50f286660f8da
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-02-22 11:39:39 -0500

    add logic to create pagination for users and only return the email, id, and username for each user in user array content

--- __test__/UserListing.spec.js
+++ __test__/UserListing.spec.js
@@ -1,5 +1,29 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true }); //clean user table before each test
+});
+
+const getUsers = () => {
+  return request(app).get('/api/1.0/users');
+};
+
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
+      inactive: i >= activeUserCount,
+    });
+  }
+};
 
 describe('Listing users', () => {
   it('returns 200 ok when there are no users in database', async () => {
@@ -14,5 +38,21 @@ describe('Listing users', () => {
       size: 10,
       totalPages: 0,
     });
+  });
+  it('returns 10 users in page content when there are 11 users in database', async () => {
+    await addUsers(11);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(10);
+  });
+  it('returns 6 users in page content when there are 6 active users and 5 inactive users in database', async () => {
+    await addUsers(6, 5);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(6);
+  });
+  it('returns only id, username, and email in content array for each user', async () => {
+    await addUsers(6, 5);
+    const response = await getUsers();
+    const user = response.body.content[0];
+    expect(Object.keys(user)).toEqual(['id', 'username', 'email']);
   });
 });
