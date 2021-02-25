ce80c948e814dd91dc51a15670745a88715e6b2c
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-02-25 03:27:48 -0500

    add more error handling for user auth

--- __test__/Auth.spec.js
+++ __test__/Auth.spec.js
@@ -12,8 +12,9 @@ beforeEach(async () => {
   await User.destroy({ truncate: true }); //clean user table before each test
 });
 
-const addUser = async () => {
-  const user = { username: 'user1', email: 'user1@mail.com', password: 'P4ssword', inactive: false };
+const activeUser = { username: 'user1', email: 'user1@mail.com', password: 'P4ssword', inactive: false };
+
+const addUser = async (user = { ...activeUser }) => {
   const hash = await bcryt.hash(user.password, 10);
   user.password = hash;
   return await User.create(user);
@@ -41,11 +42,39 @@ describe('Authentication', () => {
     expect(response.status).toBe(401);
   });
   it('returns returns proper error body when authentication fails', async () => {
-    // const nowInMillis = new Date().getTime();
-    // const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
-    // const error = response.body;
-    // expect(error.path).toBe('/api/1.0/auth');
-    // expect(error.timestamp).toBeGreaterThan(nowInMillis);
-    // expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+    const nowInMillis = new Date().getTime();
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+    const error = response.body;
+    expect(error.path).toBe('/api/1.0/auth');
+    expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+  });
+  it('returns a message when authenitcation fails because no user found', async () => {
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+    expect(response.body.message).toBe('Incorrect credentials');
+  });
+  it('returns 401 when password is wrong', async () => {
+    await addUser();
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'wrongPassword' });
+    expect(response.status).toBe(401);
+  });
+  it('returns 403 when logging in with a inactive account', async () => {
+    await addUser({ ...activeUser, inactive: true });
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+    expect(response.status).toBe(403);
+  });
+  it('returns proper error body when inactive authentication fails', async () => {
+    await addUser({ ...activeUser, inactive: true });
+    const nowInMillis = new Date().getTime();
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+    const error = response.body;
+    expect(error.path).toBe('/api/1.0/auth');
+    expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+  });
+  it('returns a message when authenitcation fail because of inactive account', async () => {
+    await addUser({ ...activeUser, inactive: true });
+    const response = await postAuthentication({ email: 'user1@mail.com', password: 'P4ssword' });
+    expect(response.body.message).toBe('Account is inactive');
   });
 });
